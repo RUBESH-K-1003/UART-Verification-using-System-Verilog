ea43be03f486c9e36c0fa58c8f717eb60134d478
RUBESH K <rubesh1003@gmail.com>
2025-11-13 20:50:50 +0530

    Implement UART generator class with message task

---
+++ uart_generator.sv
@@ -0,0 +1,20 @@
+// uart_generator.sv
+class generator;
+  mailbox gen2drv;
+
+  function new(mailbox gen2drv);
+    this.gen2drv = gen2drv;
+  endfunction
+
+  task run();
+    uart_transaction tr;
+    byte message[6] = {"S", "A", "U", "R", "A", "v"};
+    for (int i = 0; i < $size(message); i++) begin
+      tr = new();
+      tr.data = message[i];
+      tr.display("GEN");
+      gen2drv.put(tr);
+      #1000;
+    end
+  endtask
+endclass
